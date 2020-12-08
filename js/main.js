64a1f9cfd2dcabcf3a0361ed2ec3441f9485a0c1
alepaez-dev <al3_hp@hotmail.com>
2020-12-07 21:25:08 -0700

    ADD Introduction JS

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ alert("alert");
 // variables
 const NAME_KODER = "Alejandra";
 let last_name = "Hernandez";
+
+let name_koder = prompt("¿Cuál es tu nombre?");
+console.log(name_koder);
+let age_koder = prompt("edad: ");
+let resultSuma = parseInt(age_koder) + 5; //pasar a enteros
+console.log(resultSuma);
+// camelCase
+// PascalCase
+// snake_case
