84e765f1db2567e0678b29a50978e68b595acb3a
alepaez-dev <al3_hp@hotmail.com>
2020-12-17 14:19:33 -0700

    todos los ejercicios

--- js/homework_objects.js
+++ js/homework_objects.js
@@ -31,3 +31,69 @@ const arrayToObject = (array) =>
   }, {});
 
 console.log(arrayToObject(users));
+
+//  tomando el array users,
+//  hacer una funcion que reciba 2 parametros (arrayUsers, role)
+//  y retorne un array con los usuarios que cumplan con el role
+
+const filterRole = (array, role) =>
+  array.filter((element) => element.role === role);
+
+console.log(filterRole(users, "Instructor"));
+
+// ejercicio 3
+// tomando el siguiente array de objetos
+let persons = [
+  { name: "Bob", age: 30, voted: true },
+  { name: "Jake", age: 32, voted: true },
+  { name: "Kate", age: 25, voted: false },
+  { name: "Sam", age: 20, voted: false },
+  { name: "Phil", age: 21, voted: true },
+  { name: "Ed", age: 55, voted: true },
+  { name: "Tami", age: 54, voted: true },
+  { name: "Mary", age: 31, voted: false },
+  { name: "Becky", age: 43, voted: false },
+  { name: "Joey", age: 41, voted: true },
+  { name: "Jeff", age: 30, voted: true },
+  { name: "Zack", age: 19, voted: false },
+];
+
+// obtener lo siguiente
+// 1. Cantidad de personas que votaron
+// 2. Cual es el promedio de Edad de los votantes
+
+//votos solos
+const whoVote = (array) =>
+  array.reduce(
+    (accumulator, currentValue) =>
+      currentValue.voted ? accumulator + 1 : accumulator,
+    0
+  );
+console.log("votaron: ", whoVote(persons));
+
+//average solo
+const averageArray = (array) =>
+  parseInt(
+    array.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.age;
+    }, 0) / persons.length
+  );
+
+console.log("El promedio: ", averageArray(persons));
+
+//los 2 juntos
+const whoVoteAndAverage2 = (array) => {
+  let votos = 0;
+  let average = 0;
+  return array.reduce((accumulator, currentValue) => {
+    currentValue.voted ? (votos += 1) : votos;
+    average += currentValue.age / array.length;
+    return (
+      (accumulator.votos = votos),
+      (accumulator.promedio = parseInt(average)),
+      accumulator
+    );
+  }, {});
+};
+
+console.log(whoVoteAndAverage2(persons));
