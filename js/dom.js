1d5099d98402cc1135a2d564ff207ce4c38b26f7
alepaez-dev <al3_hp@hotmail.com>
2020-12-22 07:23:34 -0700

    ejercicios eventos

--- js/dom.js
+++ js/dom.js
@@ -8,13 +8,14 @@ let an = document.createElement("a");
 let an2 = document.createElement("a");
 let texto_an = document.createTextNode("item 1");
 let texto_an2 = document.createTextNode("item 2");
-let att = document.createAttribute("href");
-att.value = "#";
-let att2 = document.createAttribute("href");
-att2.value = "#";
+// let att = document.createAttribute("href");
+// att.value = "#";
+// let att2 = document.createAttribute("href");
+// att2.value = "#";
 
-an.setAttributeNode(att);
-an2.setAttributeNode(att2);
+an.setAttribute("href", "#");
+// an2.setAttributeNode(att2);
+an.setAttribute("href", "#");
 an.appendChild(texto_an);
 an2.appendChild(texto_an2);
 li.appendChild(an);
@@ -32,6 +33,61 @@ for (let i = 0; i < 2; i++) {
 }
 elementUL += `<ul>${elementLI}</ul>`;
 document.getElementById("container_2").innerHTML = elementUL;
+
+const koders = [
+  "ale",
+  "oscar",
+  "ruben",
+  "brian",
+  "alex",
+  "omar",
+  "omnaldip",
+  "raul"
+];
+
+const addElements = (selector, arrNames, numberElements = arrNames.length) => {
+  numberElements > arrNames.length ? numberElements = arrNames.length : numberElements;
+  for (let i = 0; i < numberElements; i++) {
+    document.querySelector(selector).innerHTML += `<li>Koder ${i + 1}: ${arrNames[i]}</li>`;
+  };
+};
+filterKoders = letter => {
+  let filtrados = koders.filter(element => element.startsWith(letter));
+  // return filtrados;
+  addElements("#container_3", filtrados,filtrados.length);
+};
+
+
+// addElements("#container_3", koders,15);
+
+/**
+ * const addElements = (selector,numberElements, arrNames) => {
+    let listKoders = ''
+    arrNames.forEach((koder, index) => {
+        if(index < numberElements ) {
+            listKoders += `<li>Koder ${index + 1}: ${koder}</li>`
+        }
+    })
+    document.querySelector(''+selector).innerHTML = listKoders
+}
+const kodersArr = ['ale','oscar','ruben', 'brian']
+addElements('#lista', 1, kodersArr)
+ */
+
+
+// const addElements = (selector, numberElements, arrNames) => {
+//   numberElements > arrNames.length ? (numberElements = arrNames.length) : numberElements;
+//   for (let i = 0; i < numberElements; i++) {
+//     document.getElementById(selector).innerHTML += `<li>Koder ${i + 1}: ${arrNames[i]}</li>`;
+//   };
+// };
+
+//AGREGAR CLASES A LA YA LISTA DE CLASES
+// secondElenebt.classList.add("active")
+// document.querySelector(".clase")
+// document.querySelector("#clase")
+// document.querySelector(".clase")
+// document.querySelectorAll(".clase")
 
 // let texto_title = document.createTextNode("soy un titulo");
 // let div = document.createElement("div");
