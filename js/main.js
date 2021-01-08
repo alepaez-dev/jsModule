1de050bc7eb0eaaf3f44b19f5971e0d9c09aedc6
alepaez-dev <al3_hp@hotmail.com>
2021-01-08 00:27:02 -0700

    ejercicios clase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log("Hola mundo");
+
+window.addEventListener("resize", function () {
+  width = window.innerWidth;
+
+  if (width <= 768) {
+    document.getElementById("mob").style.display = "block";
+    document.getElementById("desk").style.display = "none";
+  } else {
+    document.getElementById("mob").style.display = "none";
+    document.getElementById("desk").style.display = "block";
+  }
+});
+
+const funLogged = () => {
+  if (window.cookie.logged) {
+    console.log("loggeado");
+  } else {
+    console.log("no loggeado");
+  }
+};
+
+window.cookie = { logged: true };
+window.addEventListener("load", funLogged);
