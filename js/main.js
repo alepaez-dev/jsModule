165db8e8bc53660101a8d2a9e906ad1b667cad9b
alepaez-dev <al3_hp@hotmail.com>
2021-01-13 13:58:15 -0700

    ejercicio completo

--- js/main.js
+++ js/main.js
@@ -21,23 +21,50 @@ var requestAjax = (url, callback, method = "GET", data = {}) => {
 };
 
 // put y post RESPONSE
-const showResponse = response => {
+var showResponse = response => {
   console.log(response);
 };
 
 // put y post
+// var showUsers = response => {
+//   console.log("response", response);
+//   let users = response;
+//   let usersList = "";
+//   for (user in response) {
+//     console.log("user", user);
+//     let dataUsuario = response[user];
+//     usersList += `
+//             <li>${dataUsuario.name} ${dataUsuario.lastname} <a href="user.html?id=${user}">ver usuario</a></li>
+//         `;
+//   }
+//   document.querySelector(".list__users").innerHTML = usersList;
+// };
+
 var showUsers = response => {
-  console.log("response", response);
   let users = response;
   let usersList = "";
+  let iconDelete = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+    </svg>`;
   for (user in response) {
-    console.log("user", user);
     let dataUsuario = response[user];
     usersList += `
-            <li>${dataUsuario.name} ${dataUsuario.lastname} <a href="user.html?id=${user}">ver usuario</a></li>
+            <li class="d-flex px-3 py-2 shadow-sm mb-2 rounded justify-content-between item__user"> 
+                <div class="d-flex"> 
+                    <span class="d-flex rounded-circle icon__user" style='background-image:  url(${dataUsuario.urlPhoto})'></span> 
+                    <p class="d-flex text-left px-2">${dataUsuario.name} ${dataUsuario.lastname} </p> 
+                </div> 
+                <div class="d-flex"> 
+                    <a href="user.html?id=${user}" class="btn btn-link">Ver usuario</a> 
+                    <button class="btn btn-danger btn__delete__user" data-id="${user}" data-user="${user}" data-algo="${user}">Eliminar ${iconDelete}</button>  
+                    <a href="update-user.html?id=${user}" class="btn__update__user" data-id="${user}">Editar</a>      
+                </div> 
+            </li>
         `;
   }
   document.querySelector(".list__users").innerHTML = usersList;
+  // deleteUserBtns();
 };
 
 //INDEX
@@ -54,6 +81,8 @@ if (
     showUsers,
     "GET"
   );
+  const list = document.querySelectorAll(".btn__update__user");
+  console.log("list", list);
 }
 
 var printUser = arrData => {
@@ -88,12 +117,46 @@ if (
   );
 }
 
-//NEWUSER
+// create new user
+var redirectToUsers = res => {
+  window.location.pathname =
+    "file:///C:/Users/al3_h/Downloads/Prueba/JavaScript/jsModule/index.html";
+};
+
+//UPDATE USER
+if (
+  window.location.pathname === "/update-user.html" ||
+  window.location.pathname ===
+    "/C:/Users/al3_h/Downloads/Prueba/JavaScript/jsModule/update-user.html"
+) {
+  const updateUserBtns = event => {
+    let url = new URLSearchParams(window.location.search);
+    let id = url.get("id");
+    event.preventDefault();
+    let nameSave = document.getElementById("name").value;
+    let lastnameSave = document.getElementById("lastname").value;
+    let url_photoSave = document.getElementById("url_photo").value;
+
+    requestAjax(
+      `https://koders1gpython-default-rtdb.firebaseio.com/alepaez/users/${id}.json`,
+      showResponse,
+      "PATCH",
+      {
+        lastname: lastnameSave,
+        name: nameSave,
+        urlPhoto: url_photoSave,
+      }
+    );
+  };
+  document.getElementById("btnSaves").addEventListener("click", updateUserBtns);
+}
+
 if (
   window.location.pathname === "/newuser.html" ||
   window.location.pathname ===
     "/C:/Users/al3_h/Downloads/Prueba/JavaScript/jsModule/newuser.html"
 ) {
+  //NEWUSER
   console.log("entra a la funcion if del newuser.html");
 
   //Delete
@@ -136,3 +199,5 @@ if (
   document.getElementById("btnSave").addEventListener("click", clickGuardad);
   document.getElementById("btnDelete").addEventListener("click", clickDelete);
 }
+
+//delete users button
